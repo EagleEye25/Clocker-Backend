d0d1fce04eaf948293801572fa23e133c9dc5d6b
EagleEye25 <jasonheybers@gmail.com>
2019-06-20 09:52:50 +0200

    created initial tables

--- app/routes/router.js
+++ app/routes/router.js
@@ -5,7 +5,6 @@ const express = require('express');
 const router = express.Router();
 
 /* API routes */
-router.use('/car', require('./api/carRoutes'));
 router.use('/employee', require('./api/employeeRoutes'));
 
 module.exports = router;
