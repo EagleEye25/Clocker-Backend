ecb91669bccbb9e8a9dbc3dfa9f3aa7ef984a562
EagleEye25 <jasonheybers@gmail.com>
2019-06-24 18:09:49 +0200

    header changes

--- index.js
+++ index.js
@@ -2,17 +2,34 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-
+const cors = require("cors");
 /* Database configuration */
 const database = require('./app/config/dbconfig');
+
+const whitelist = ['http://localhost'];
+const corsOptions = {
+	origin (origin, callback) {
+		for (const url of whitelist){
+			console.log("HEREH I AM MOTHER FUCKER!!!", url, origin);
+			if (!origin || origin.startsWith(url)) {
+				callback(null, true);
+			} else {
+				callback(new Error('Not allowed by CORS'));
+			}
+		}
+	}
+};
 
 /* Init database */
 database.init();
 
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
+
 /* Init server listening */
 const port = process.argv[2] || 3000;
 app.listen(port, function () {
-    console.log("Server listening on port : " + port);
+	console.log("Server listening on port : " + port);
 });
 
 /* Express configuration */
